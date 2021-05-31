94aba2c680340ea1153a92cba828eca7797cc502
Matt Lenz <matt@mattlenz.com>
2021-05-31 10:28:38 +1000

    Hyper deployment

--- src/index.html.js
+++ src/index.html.js
@@ -1,16 +1,20 @@
+require('dotenv').config()
+
 const manifest = JSON.parse(require('fs').readFileSync(0, 'utf8'))
 const entry = manifest['src/index.js']
+
+const base = process.env.DEPLOY_DRIVE || ''
 
 console.log(`
 <!doctype html>
 <html>
 <head>
   <title>Means</title>
-  ${entry.css.map(css => `<link rel="stylesheet" href="/.ui/${css}" />`).join('\n')}
-  <script type="module" src="/.ui/${entry.file}"></script>
+  ${entry.css.map(css => `<link rel="stylesheet" href="${base}/.ui/${css}" />`).join('\n')}
+  <script type="module" src="${base}/.ui/${entry.file}"></script>
 </head>
 <body>
   <div id="app"></div>
 </body>
 </html>
-`)
+`.trim())
